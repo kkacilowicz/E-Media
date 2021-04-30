d0f41026af62ed1295b10b973b89bb10d2fd7acd
WJakubowska <nika99_11@o2.pl>
2021-04-30 23:48:06 +0200

    wyszukiwanie pozycji chunku PLTE

--- PNG.py
+++ PNG.py
@@ -5,9 +5,28 @@ filename = "images4.png"
 png = []
 
 png = read.readPNG(filename)
+'''
 IHDR = Chunk_IHDR.getIHDRChunk(png)
 if IHDR == -1:
    print("Chunk IHDR nie istenieje")
 else:
    IHDR.display()
+'''
 
+
+def searchPLTE(png):
+    length = len(png)
+    position = 0
+    for i in range(0, length):
+        if png[i] == b'P':
+            if png[i + 1] == b'L':
+                if png[i + 2] == b'T':
+                    if png[i + 3] == b'E':
+                        position = i
+                        break
+    return position
+
+
+print(searchPLTE(png))
+
+print()
