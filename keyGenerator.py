73262c08a65628caaf52282fcdddb995fdd4f0f3
WJakubowska <nika99_11@o2.pl>
2021-06-12 22:55:49 +0200

    key generator done

--- keyGenerator.py
+++ keyGenerator.py
@@ -1,88 +1,100 @@
-import os
 import random
+import secrets
 
 
-def test_Miller_Rabin(n, k):
-    # n - nieparzysta liczba naturalna do przetestowania
-    # k - parametr określający dokładność testu
+class Key:
+    def __init__(self):
+        self.p = 0
+        self.q = 0
+        self.n = 0
+        self.e = 0
+        self.d = 0
+        self.p_q = 0
+        self.keysize = 1024
 
-    if n == 2 or n == 3:
+    def test_Miller_Rabin(self, n, k):
+        # n - nieparzysta liczba naturalna do przetestowania
+        # k - parametr określający dokładność testu
+
+        if n == 2 or n == 3:
+            return True
+
+        if n % 2 == 0 or n < 2:
+            return False
+
+        r = 0
+        s = n - 1
+
+        while s % 2 == 0:
+            r = r + 1
+            s = s // 2
+        for i in range(k):  # k to liczba różnych testowanych wartości  a
+            a = random.randrange(2, n - 1)  # wybór losowej liczby a z zbioru {2, ..., n -1 }
+            x = pow(a, s, n)  # a ^ s mod n
+            if x == 1 or x == n - 1:
+                continue
+            for j in range(r - 1):
+                x = pow(x, 2, n)  # x^2 mod n
+                if x == n - 1:
+                    break
+            else:
+                return False
         return True
 
-    if n % 2 == 0 or n < 2:
-        return False
+    def generate_p(self):
+        p = 0
 
-    r = 0
-    s = n - 1
+        while not self.test_Miller_Rabin(p, 200):  # dopóki nie znajdzie się w 100 iteracjach
+            p = secrets.randbelow(2 ** (self.keysize // 2))
 
-    while s % 2 == 0:
-        r = r + 1
-        s = s // 2
-    for i in range(k): # k to liczba różnych testowanych wartości  a
-        a = random.randrange(2, n - 1) # wybór losowej liczby a z zbioru {2, ..., n -1 }
-        x = pow(a, s, n) # a ^ s mod n
-        if x == 1 or x == n - 1:
-            continue
-        for j in range(r - 1):
-            x = pow(x, 2, n) # x^2 mod n
-            if x == n - 1:
-                break
+        self.p = p
+
+    def generate_q(self):
+        q = 0
+
+        while not self.test_Miller_Rabin(q, 200):  # dopóki nie znajdzie się w 100 iteracjach
+            q = secrets.randbelow(2 ** (self.keysize // 2))
+
+        self.q = q
+
+    def GCD(self, a, b):  # GCD - gratest common divisor - największy wspólny dzielnik
+                          # sprawdzenie czy liczba względnie pierwsza
+        if b != 0:        # liczba względnie pierwsza jak największy wspólny dzielnik to 1
+            return self.GCD(b, a % b)
         else:
-            return False
-    return True
+            return a
 
+    def generate_e(self):
+        n = self.p * self.q
+        self.n = n
+        size_n = n.bit_length()
 
-def generate_p():
+        self.p_q = (self.p - 1) * (self.q - 1)
 
-    size = 100 # potem trzeba zmienić size na 550
+        while True:
+            e = random.randrange(2 ** (size_n-1), 2 ** size_n)
+            if self.GCD(e, self.p_q) == 1 and e < self.n:
+                break
+        self.e = e
 
-    p = b'\x00' # na początku p to 0
-    while not test_Miller_Rabin(int.from_bytes(p, byteorder='big', signed=True), 100): # dopóki nie znajdzie się w 100 iteracjach
-        print("szukam p ")
-        p = os.urandom(size)                                                           # że p liczba pierwsza to losuj nowe
+    def generate_d(self):
+        d = pow(self.e, -1, self.p_q)
+        assert self.GCD(d, self.n) == 1, " incorrect value of d "
+        self.d = d
 
-    return p
+    def generate_Keys(self):
+        self.generate_p()
+        self.generate_q()
+        self.generate_e()
+        self.generate_d()
 
-def generate_q():
+        publicKey = ( self.e, self.n)
+        privateKey = (self.d, self.n)
 
-    size = 80 # później trzeba zmienić na 480
-    q = b'\x00'  # na początku p to 0
-    while not test_Miller_Rabin(int.from_bytes(q, byteorder='big', signed=True),
-                                100):  # dopóki nie znajdzie się w 100 iteracjach
-        q = os.urandom(size)  # że p liczba pierwsza to losuj nowe
+        return publicKey, privateKey
 
-    return q
-
-def NWD(a,b): # sprawdzenie czy liczba względnie pierwsza
-    if b != 0:  # liczba względnie pierwsza jak największy wspólny dzielnik to 1
-        return NWD(b, a % b)
-    else:
-        return a
-
-def generate_e():
-    p = generate_p()
-    p_ = int.from_bytes(generate_p(), byteorder='big', signed=True)
-    print("mam p ", p)
-    q = generate_q()
-    q_ = int.from_bytes(generate_q(), byteorder='big', signed=True)
-    print("mam q ", q)
-    tmp = (p_ - 1)*(q_ - 1)
-    n = p_ * q_
-    print("dł p: ", p.bit_length())
-    print("dł q: ", q.bit_length())
-    print("dł n: ", n.bit_length())
-    # print("szukam e")
-    # while True:
-    #     e = os.urandom(1024)
-    #     print("losuje ....")
-    #     e_int = int.from_bytes(e, byteorder='big', signed=True)
-    #     if (NWD(e_int, tmp) == 1 and e_int < tmp):
-    #         print("ta dam")
-    #     if (NWD(e_int, tmp) == 1 and e_int < n):
-    #         print("break")
-    #         break;
-
-    return e
-
-e = generate_e()
-print(e)
+# key = Key()
+# e = key.generate_Keys()
+#
+# print(e[0][0])
+# print(e[1])
